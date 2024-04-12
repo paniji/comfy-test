c2730d5a0a1738bca42c4090513339dcac11177f
AWS CodeBuild <pani-aws-codebuild@ap.com>
2024-04-11 21:16:10 -0400

    full commit

--- assets/utils.py
+++ assets/utils.py
@@ -19,8 +19,9 @@ def prompt_workflow(tmplt):
     # Update environment specifc properties
     workflow_json = set_env_props(workflow_json, tmplt)
 
-    print(workflow_json)
-    return workflow_json
+    # Update scene and trigger comfy (scene by scene)
+    scene_data(workflow_json, tmplt)
+    #return workflow_json
 
 def set_env_props(wf, tmplt):
     ret_wf = wf
@@ -37,6 +38,51 @@ def set_env_props(wf, tmplt):
 
     return ret_wf
 
+def scene_data(wf_data, tmplt):
+    project_config = get_file("project/template.yml")
+
+    #if 'scenes' in project_config:
+    scenes = project_config['scenes']
+
+    for scn in scenes:
+
+        scn_config = get_file("project/scenes/" + scn + "/template.yml")
+
+        scenes2 = scn_config['scenes']
+
+        for scn2 in scenes2:
+            scn2_config = get_file("project/scenes/" + scn + "/scenes/" + scn2 + "/template.yml") # + ".yml")
+
+            #print(scn2_config)
+            scenes3 = scn2_config['scenes']
+            for scn3 in scenes3:
+                #print(scn3)
+                scene = get_file("project/scenes/" + scn + "/scenes/" + scn2 + "/" + scn3 + ".yml")
+                #print(scene)
+                pos_promt = project_config['pre_positive_prompt'] + ", " + scene['positive_prompt'] + ", " + project_config['post_positive_prompt']
+                neg_promt = project_config['pre_negative_prompt'] + ", " + scene['negative_prompt'] + ", " + project_config['post_negative_prompt']
+                length = scene['length_in_secs']
+                ## work on hard code for txt2vid
+                if tmplt == 'txt2vid':
+                    context_length = length * project_config[tmplt]['101']['frame_rate']
+                    wf_data['94'][INPUT]['context_length'] = context_length
+                    wf_data['3'][INPUT]['text'] = pos_promt
+                    wf_data['6'][INPUT]['text'] = neg_promt
+                    wf_data['101'][INPUT]['frame_rate'] = project_config[tmplt]['101']['frame_rate']
+
+                    file_name = scn + "/" + scn2 + "/" + scn3
+                    print(project_config[tmplt]['101']['filename_prefix'] + "/" + file_name)
+                    wf_data['101'][INPUT]['filename_prefix'] = project_config[tmplt]['101']['filename_prefix'] + "/" + file_name
+                    # print("-----------------")
+                    # print(wf_data)
+                    # print("-----------------")
+
+                    p = {"prompt": wf_data}
+                    data = json.dumps(p).encode('utf-8')
+
+                    # Trigger comfy
+                    queue_prompt(data)
+
 def set_prompt(workflow_json, node, prompt):
     prompt_node = workflow_json[node]
 
@@ -51,24 +97,27 @@ def get_file(config_file):
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
     file = dir_path + "/" + config_file
-    print(file)
+    #print(file)
 
-    with open(file) as file:
-        try:
-            config = yaml.safe_load(file)
-        except yaml.YAMLError as exc:
-            print("Error while loading template file")
-    return config
+    try:
+        with open(file) as file:
+            try:
+                config = yaml.safe_load(file)
+                return config
+            except yaml.YAMLError as exc:
+                print("Error while loading template file")
+                return exc
 
-def queue_prompt(template):
+    except FileNotFoundError as exc:
+        print("File not found.")
+        return exc
+        
+
+def queue_prompt(data):
 
     # Get the value of an environment variable
     COMFY = os.environ.get('COMFY')
-    
     print("Using Comfy URL: ", COMFY)
-
-    p = {"prompt": prompt_workflow(template)}
-    data = json.dumps(p).encode('utf-8')
 
     #Get the value of an environment variable
     session = requests.Session()
@@ -77,3 +126,6 @@ def queue_prompt(template):
     result = session.post(url=comfy_url, data=data)
     print(result)
 
+# def trigger_workflow(template):
+#     p = {"prompt": prompt_workflow(template)}
+#     data = json.dumps(p).encode('utf-8')
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import requests
 
 import assets.utils as utils
 
-utils.queue_prompt("txt2vid")
+utils.prompt_workflow("txt2vid")
 # # ======================================================================
 # # This function sends a prompt workflow to the specified URL 
 # # (http://127.0.0.1:8188/prompt) and queues it on the ComfyUI server
